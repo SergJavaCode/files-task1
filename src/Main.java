65cdcd3a51135ac08c08da016bf968efcd4a3de1
Serg <freekey@list.ru>
2023-08-31 16:33:02 +0400

    fixed bag 1

--- src/Main.java
+++ src/Main.java
@@ -1,80 +1,59 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    private static FileWriter writerTempTxt;
+    static StringBuilder logFile = new StringBuilder();
 
     public static void main(String[] args) {
-        StringBuilder logFile = new StringBuilder();
+        List<String> listPathDir = new ArrayList<>();
+        List<String> listPathFile = new ArrayList<>();
 
-        File srcDir = new File("c://Games/src");
-        File resDir = new File("c://Games/res");
-        File savegamesDir = new File("c://Games/savegames");
-        File tempDir = new File("c://Games/temp");
-        logFile.append(srcDir.mkdir()
-                ? "Папка " + srcDir.toString() + " успешно создана. \n"
-                : "Папка " + srcDir.toString() + " не создана. \n");
-        logFile.append(resDir.mkdir()
-                ? "Папка " + resDir.toString() + " успешно создана. \n"
-                : "Папка " + resDir.toString() + " не создана. \n");
-        logFile.append(savegamesDir.mkdir()
-                ? "Папка " + savegamesDir.toString() + " успешно создана. \n"
-                : "Папка " + savegamesDir.toString() + " не создана. \n");
-        logFile.append(tempDir.mkdir()
-                ? "Папка " + tempDir.toString() + " успешно создана. \n"
-                : "Папка " + tempDir.toString() + " не создана. \n");
+        listPathDir.add("c://Games/src");
+        listPathDir.add("c://Games/res");
+        listPathDir.add("c://Games/savegames");
+        listPathDir.add("c://Games/temp");
+        listPathDir.add("c://Games/src/main");
+        listPathDir.add("c://Games/src/test");
+        listPathFile.add("c://Games/src/main/Main.java");
+        listPathFile.add("c://Games/src/main/Utils.java");
+        listPathDir.add("c://Games/res/drawables");
+        listPathDir.add("c://Games/res/vectors");
+        listPathDir.add("c://Games/res/icons");
+        listPathFile.add("c://Games/temp/temp.txt");
+        for (String s : listPathDir) {
+            makeDir(s);
+        }
+        for (String s : listPathFile) {
+            makeFile(s);
+        }
 
-        File srcMainDir = new File("c://Games/src/main");
-        File srcTestDir = new File("c://Games/src/test");
-        logFile.append(srcMainDir.mkdir()
-                ? "Папка " + srcMainDir.toString() + " успешно создана. \n"
-                : "Папка " + srcMainDir.toString() + " не создана. \n");
-        logFile.append(srcTestDir.mkdir()
-                ? "Папка " + srcTestDir.toString() + " успешно создана. \n"
-                : "Папка " + srcTestDir.toString() + " не создана. \n");
+        writeFile("c://Games/temp/temp.txt");
 
+    }
 
-        File fileMainJava = new File("c://Games/src/main/Main.java");
-        File fileUtilsJava = new File("c://Games/src/main/Utils.java");
+    private static void makeDir(String path) {
+        File makedDir = new File(path);
+        logFile.append(makedDir.mkdir()
+                ? "Папка " + makedDir.toString() + " успешно создана. \n"
+                : "Папка " + makedDir.toString() + " не создана. \n");
+    }
+
+    private static void makeFile(String path) {
+        File makedFile = new File(path);
         try {
-            logFile.append(fileMainJava.createNewFile()
-                    ? "Файл " + fileMainJava.toString() + " успешно создан. \n"
-                    : "Файл " + fileMainJava.toString() + " не создан. \n");
+            logFile.append(makedFile.createNewFile()
+                    ? "Файл " + makedFile.toString() + " успешно создан. \n"
+                    : "Файл " + makedFile.toString() + " не создан. \n");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        try {
-            logFile.append(fileUtilsJava.createNewFile()
-                    ? "Файл " + fileUtilsJava.toString() + " успешно создан. \n"
-                    : "Файл " + fileUtilsJava.toString() + " не создан. \n");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    }
 
-        File resDrawablesDir = new File("c://Games/res/drawables");
-        File resVectorsDir = new File("c://Games/res/vectors");
-        File resIconsDir = new File("c://Games/res/icons");
-        logFile.append(resDrawablesDir.mkdir()
-                ? "Папка " + resDrawablesDir.toString() + " успешно создана. \n"
-                : "Папка " + resDrawablesDir.toString() + " не создана. \n");
-        logFile.append(resVectorsDir.mkdir()
-                ? "Папка " + resVectorsDir.toString() + " успешно создана. \n"
-                : "Папка " + resVectorsDir.toString() + " не создана. \n");
-        logFile.append(resIconsDir.mkdir()
-                ? "Папка " + resIconsDir.toString() + " успешно создана. \n"
-                : "Папка " + resIconsDir.toString() + " не создана. \n");
-
-        File fileTempTxt = new File("c://Games/temp/temp.txt");
-        try {
-            logFile.append(fileTempTxt.createNewFile()
-                    ? "Файл " + fileTempTxt.toString() + " успешно создан. \n"
-                    : "Файл " + fileTempTxt.toString() + " не создан. \n");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        try (FileWriter writerTempTxt = new FileWriter("c://Games/temp/temp.txt", false)) {
+    private static void writeFile(String path) {
+        try (FileWriter writerTempTxt = new FileWriter(path, false)) {
             writerTempTxt.write(logFile.toString());
             writerTempTxt.flush();
         } catch (IOException e) {
